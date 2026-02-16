a2fd120260fa1a34295621761b97be9590b36537
Johan Viancha <vianchajohan@gmail.com>
2026-02-15 22:07:12 -0500

    ajustar api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-  SERVER_HOST: "https://cafe-acacias.up.railway.app",
-  API_URL: "https://cafe-acacias-ecommerce.up.railway.app/api",
+  SERVER_HOST: "https://cafe-acacias-ecommerce.onrender.com",
+  API_URL: "https://cafe-acacias-ecommerce.onrender.com/api",
   ENDPOINTS: {
     AUTH: {
       REGISTER: "auth/local/register",
